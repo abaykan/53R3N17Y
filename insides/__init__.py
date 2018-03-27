e1d143acaad63c87be59c581218dd2ca464f7c3a
Abay <hello@abaykan.com>
2018-03-27 18:47:18 +0700

    Add files via upload

---
+++ insides/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*- 
+"""
+     _-----_
+    |       |    .------------------------.
+    |--(o)--|    |   Welcome to 53R3N17Y  |
+   `---------´   |       abaykan.com      |
+    ( _´U`_ )    '------------------------'
+    /___A___\
+
+     |  ~  |
+   __'.___.'__
+ ´   `  |° ´ Y `
+"""
+
+from colors 	import *
+from functions 	import *
+from banner 	import banner as Banner
+from footer 	import footer as Footer
---
+++ insides/banner.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*- 
+"""
+     _-----_
+    |       |    .------------------------.
+    |--(o)--|    |   Welcome to 53R3N17Y  |
+   `---------´   |       abaykan.com      |
+    ( _´U`_ )    '------------------------'
+    /___A___\
+
+     |  ~  |
+   __'.___.'__
+ ´   `  |° ´ Y `
+"""
+
+from colors import c, w
+import colorama
+from colorama import Fore, Back, Style
+
+banner = Fore.CYAN + """
+     _-----_
+    |       |    .------------------------.
+    |--(o)--|    |   Welcome to 53R3N17Y  |
+   `---------´   |       abaykan.com      |
+    ( _´U`_ )    '------------------------'
+    /___A___\
+
+     |  ~  |
+   __'.___.'__
+ ´   `  |° ´ Y `
+""" + w
